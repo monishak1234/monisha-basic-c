cb3213c70c912db39dc861bc77e8036ddbdf62a3
monishak1234 <monishak23sit@vetias.ac.in>
2025-05-16 11:05:50 +0530

    Add files via upload

---
+++ avr.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d%d",&X,&Y);
+    if(X>Y)
+    {
+        printf("x is greater than y");
+    }
+    else if(y>x) printf("y is greater than x");
+    else print("both are equal");
+    return 0;
+}
